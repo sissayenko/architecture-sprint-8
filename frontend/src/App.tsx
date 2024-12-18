2fb6cfd5ded694b71de68fee3eab8a54414fbf4e
Sergey Issayenko <sisaenko@alfabank.ru>
2024-12-19 02:38:33 +0500

    sprint8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
-import Keycloak, { KeycloakConfig } from 'keycloak-js';
+import Keycloak, { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js';
 import ReportPage from './components/ReportPage';
 
 const keycloakConfig: KeycloakConfig = {
-  url: process.env.REACT_APP_KEYCLOAK_URL,
-  realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
+    url: process.env.REACT_APP_KEYCLOAK_URL,
+    realm: process.env.REACT_APP_KEYCLOAK_REALM || "",
+    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || ""
 };
+
+const keycloakInitOptions: KeycloakInitOptions = {
+    onLoad: 'check-sso',
+    pkceMethod: 'S256',
+    checkLoginIframe: false
+}
 
 const keycloak = new Keycloak(keycloakConfig);
 
+// https://stackoverflow.com/questions/75256010/react-keycloak-keeps-refreshing
 const App: React.FC = () => {
-  return (
-    <ReactKeycloakProvider authClient={keycloak}>
-      <div className="App">
-        <ReportPage />
-      </div>
-    </ReactKeycloakProvider>
-  );
+    return (
+        // <ReactKeycloakProvider authClient={keycloak}> 
+        <ReactKeycloakProvider authClient={keycloak} initOptions={keycloakInitOptions}>
+            <div className="App">
+                <React.StrictMode>
+                    <ReportPage />
+                </React.StrictMode>
+            </div>
+        </ReactKeycloakProvider >
+    );
 };
 
 export default App;
